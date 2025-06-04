11bfe1fbfa96fd277e758b5e985d6555235595ba
M1n01 <abe.minato.bz@gmail.com>
2025-06-04 14:02:17 +0900

    refactor: Clean up import statements and improve code formatting for better readability in App.tsx and setup.ts

--- App.tsx
+++ App.tsx
@@ -1,49 +1,57 @@
-import '@sinonjs/text-encoding'
-import 'react-native-get-random-values'
-import '@ethersproject/shims'
+import "@ethersproject/shims";
+import "@sinonjs/text-encoding";
+import "react-native-get-random-values";
 
-import React, { useEffect, useState } from 'react'
-import { SafeAreaView, ScrollView, View, Text, Button } from 'react-native'
+import React, { useEffect, useState } from "react";
+import { Button, SafeAreaView, ScrollView, Text, View } from "react-native";
 
-// Import the agent from our earlier setup
-import { agent } from './setup'
 // import some data types:
-import { IIdentifier } from '@veramo/core'
+import { IIdentifier } from "@veramo/core";
+// Import the agent from our earlier setup
+import { agent } from "./setup";
 
 const App = () => {
-  const [identifiers, setIdentifiers] = useState<IIdentifier[]>([])
+  const [identifiers, setIdentifiers] = useState<IIdentifier[]>([]);
 
   // Add the new identifier to state
   const createIdentifier = async () => {
     const _id = await agent.didManagerCreate({
-      provider: 'did:peer',
+      provider: "did:peer",
       options: {
         num_algo: 2,
-        service: { id: '1', type: 'DIDCommMessaging', serviceEndpoint: "did:web:dev-didcomm-mediator.herokuapp.com", description: 'for messaging' } 
-      }
-    })
-    setIdentifiers((s) => s.concat([_id]))
-  }
+        service: {
+          id: "1",
+          type: "DIDCommMessaging",
+          serviceEndpoint: "did:web:dev-didcomm-mediator.herokuapp.com",
+          description: "for messaging",
+        },
+      },
+    });
+    setIdentifiers((s) => s.concat([_id]));
+  };
 
   // Check for existing identifers on load and set them to state
   useEffect(() => {
     const getIdentifiers = async () => {
-      const _ids = await agent.didManagerFind()
-      setIdentifiers(_ids)
+      const _ids = await agent.didManagerFind();
+      setIdentifiers(_ids);
 
       // Inspect the id object in your debug tool
-      console.log('_ids:', _ids)
-    }
+      console.log("_ids:", _ids);
+    };
 
-    getIdentifiers()
-  }, [])
+    getIdentifiers();
+  }, []);
 
   return (
     <SafeAreaView>
       <ScrollView>
         <View style={{ padding: 20 }}>
-          <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Identifiers</Text>
-          <Button onPress={() => createIdentifier()} title={'Create Identifier'} />
+          <Text style={{ fontSize: 30, fontWeight: "bold" }}>Identifiers</Text>
+          <Button
+            onPress={() => createIdentifier()}
+            title={"Create Identifier"}
+          />
           <View style={{ marginBottom: 50, marginTop: 20 }}>
             {identifiers && identifiers.length > 0 ? (
               identifiers.map((id: IIdentifier) => (
@@ -58,7 +66,7 @@ const App = () => {
         </View>
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- setup.ts
+++ setup.ts
@@ -1,55 +1,72 @@
 // imports:
 // Core interfaces
-import { createAgent, IDataStore, IDataStoreORM, IDIDManager, IKeyManager, IResolver } from '@veramo/core'
+import {
+  IDIDManager,
+  IDataStore,
+  IDataStoreORM,
+  IKeyManager,
+  createAgent,
+} from "@veramo/core";
 
 // Core identity manager plugin. This allows you to create and manage DIDs by orchestrating different DID provider packages.
 // This implements `IDIDManager`
-import { DIDManager } from '@veramo/did-manager'
+import { DIDManager } from "@veramo/did-manager";
 
 // Core key manager plugin. DIDs use keys and this key manager is required to know how to work with them.
 // This implements `IKeyManager`
-import { KeyManager } from '@veramo/key-manager'
+import { KeyManager } from "@veramo/key-manager";
 
 // This plugin allows us to create and manage `did:peer` DIDs (used by DIDManager)
-import { PeerDIDProvider } from '@veramo/did-provider-peer'
+import { PeerDIDProvider } from "@veramo/did-provider-peer";
 
 // A key management system that uses a local database to store keys (used by KeyManager)
-import { KeyManagementSystem, SecretBox } from '@veramo/kms-local'
+import { KeyManagementSystem, SecretBox } from "@veramo/kms-local";
 
 // Storage plugin using TypeORM to link to a database
-import { Entities, KeyStore, DIDStore, migrations, PrivateKeyStore } from '@veramo/data-store'
+import {
+  DIDStore,
+  Entities,
+  KeyStore,
+  PrivateKeyStore,
+  migrations,
+} from "@veramo/data-store";
 
 // TypeORM is installed with '@veramo/data-store'
-import { DataSource } from 'typeorm'
+import { DataSource } from "typeorm";
 
-const DB_ENCRYPTION_KEY = '29739248cad1bd1a0fc4d9b75cd4d2990de535baf5caadfdf8d8f86664aa830c'
+const DB_ENCRYPTION_KEY =
+  "29739248cad1bd1a0fc4d9b75cd4d2990de535baf5caadfdf8d8f86664aa830c";
 
-let dbConnection = new DataSource({
-  type: 'expo',
-  driver: require('expo-sqlite'),
-  database: 'veramo.sqlite',
+const dbConnection = new DataSource({
+  type: "expo",
+  driver: require("expo-sqlite"),
+  database: "veramo.sqlite",
   migrations: migrations,
   migrationsRun: true,
-  logging: ['error', 'info', 'warn'],
+  logging: ["error", "info", "warn"],
   entities: Entities,
-}).initialize()
+}).initialize();
 
-export const agent = createAgent<IDIDManager & IKeyManager & IDataStore & IDataStoreORM>({
+export const agent = createAgent<
+  IDIDManager & IKeyManager & IDataStore & IDataStoreORM
+>({
   plugins: [
     new KeyManager({
       store: new KeyStore(dbConnection),
       kms: {
-        local: new KeyManagementSystem(new PrivateKeyStore(dbConnection, new SecretBox(DB_ENCRYPTION_KEY))),
+        local: new KeyManagementSystem(
+          new PrivateKeyStore(dbConnection, new SecretBox(DB_ENCRYPTION_KEY)),
+        ),
       },
     }),
     new DIDManager({
       store: new DIDStore(dbConnection),
-      defaultProvider: 'did:peer',
+      defaultProvider: "did:peer",
       providers: {
-        'did:peer': new PeerDIDProvider({
-          defaultKms: 'local'
+        "did:peer": new PeerDIDProvider({
+          defaultKms: "local",
         }),
       },
     }),
   ],
-})
+});
